3840f64bd4790dcc350c9a05cb68f8e10e71a4c4
Connor Adams <connorvadams@gmail.com>
2023-10-24 11:47:19 -0600

    Submits lab.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagrams = []
+        for new_word in list:
+            if sorted([letter for letter in new_word]) == sorted([letter for letter in self.word]):
+                anagrams.append(new_word)
+        return anagrams
+
+listen = Anagram('listen')
+
+print(listen.match(["listen", "silent", "hippopotamus"]))
